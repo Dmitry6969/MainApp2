acfae288766a728fdb3e3c1fceaff90e9fb0b81d
Dmitry6969 <73423142+Dmitry6969@users.noreply.github.com>
2021-10-05 13:20:51 +0300

    Add files via upload

---
+++ LibMas/LibMas/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace LibMas
+{
+    public class ClassMas
+    {
+        public static void InflateMas(int i, int max, out int[] mas)//функция заполнения массива числами
+        {
+            mas = new int[i];
+            Random rnd = new Random();
+
+            for (int j = 0; j < i; j++)
+            {
+                mas[j] = rnd.Next(1,max);
+            }
+        }
+
+        public static void ClearMas(int[] mas, out int[] masc)//фунция очистки массива
+        {
+            int i = mas.Length;
+            masc = new int[i];
+
+            for (int j = 0; j < i; j++)
+            {
+                masc[j] = 0;
+            }
+        }
+
+        
+    }
+}
---
+++ Lib_9p2/Lib_2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_2
+{
+    public class Class2
+    {
+        public static int YmnozhMas(int[] mas)//функция умножения всех элементов массива
+        {
+            int i = mas.Length;
+            int proizv = 1;
+            for (int j = 0;j < i;j++)
+            {
+                proizv *= mas[j];
+            }
+
+            return proizv;
+        }
+    }
+}
